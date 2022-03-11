6ff940265212868826ea73af32bcaeadeda562c6
iamb33k <benkline33@gmail.com>
2022-03-11 17:00:04 -0500

    done

--- javascript.js
+++ javascript.js
@@ -1,27 +1,47 @@
 const container = document.getElementById("container");
-let gridCount = 16
+let gridCount = 16;
 let totalCount = gridCount * gridCount;
 let div = [];
 
+//build initial page
 for (let i = 0; i < totalCount; i++){
         div[i] = document.createElement("div");
         div[i].className = "square";
+        div[i].style.height = `${640/gridCount}px`;
+        div[i].style.width = `${640/gridCount}px`;
         container.appendChild(div[i]);
     }
 
+//add paint class (color squares)
 function paint(e){
     this.classList.add('paint');
 }
 
+//function to run on click of reset button
 function reset(e){
+    removeAllChildNodes(container);
     let gridCount = prompt("Choose square amount between 16 and 100");
-    // totalCount = gridCount * gridCount;
-    divs.forEach((div) => div.classList.remove('paint'));
+    let totalCount = gridCount * gridCount;
+    for (let i = 0; i < totalCount; i++){
+        div[i] = document.createElement("div");
+        div[i].className = "square";
+        div[i].style.height = `${640/gridCount}px`;
+        div[i].style.width = `${640/gridCount}px`;
+        container.appendChild(div[i])
+    }
+    //reapply event listeners
+    let divs = document.querySelectorAll('.square')
+    divs.forEach((div) => div.classList.remove('paint'));  
+    divs.forEach((div)=>{div.addEventListener('mouseenter', paint);});
 }
 
-const divs = document.querySelectorAll('.square')
-divs.forEach((div)=>{
-    div.addEventListener('mouseenter', paint);
-});
+//removes all squares from container
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
+let divs = document.querySelectorAll('.square')
+divs.forEach((div)=>{div.addEventListener('mouseenter', paint);});
 document.getElementById("reset").addEventListener('click', reset);
