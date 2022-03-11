fb50f567d395a64bf995c959b5f80d976e77bc54
iamb33k <benkline33@gmail.com>
2022-03-10 20:54:54 -0500

    all but sizing

--- javascript.js
+++ javascript.js
@@ -1,21 +1,27 @@
 const container = document.getElementById("container");
-let gridCount = 16;
+let gridCount = 16
 let totalCount = gridCount * gridCount;
 let div = [];
 
 for (let i = 0; i < totalCount; i++){
-    div[i] = document.createElement("div");
-    div[i].className = "square";
-    container.appendChild(div[i]);
-}
+        div[i] = document.createElement("div");
+        div[i].className = "square";
+        container.appendChild(div[i]);
+    }
 
 function paint(e){
     this.classList.add('paint');
+}
 
-
+function reset(e){
+    let gridCount = prompt("Choose square amount between 16 and 100");
+    // totalCount = gridCount * gridCount;
+    divs.forEach((div) => div.classList.remove('paint'));
 }
 
 const divs = document.querySelectorAll('.square')
 divs.forEach((div)=>{
     div.addEventListener('mouseenter', paint);
 });
+
+document.getElementById("reset").addEventListener('click', reset);
