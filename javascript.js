438d11bb6196e6fe609ba676a83ff13c8a0012a9
iamb33k <benkline33@gmail.com>
2022-03-10 19:16:09 -0500

    added hover

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
 const container = document.getElementById("container");
 let gridCount = 16;
-let totalDivs = gridCount * gridCount;
+let totalCount = gridCount * gridCount;
 let div = [];
 
-for (let i = 0; i < totalDivs; i++){
+for (let i = 0; i < totalCount; i++){
     div[i] = document.createElement("div");
     div[i].className = "square";
     container.appendChild(div[i]);
 }
 
+function paint(e){
+    this.classList.add('paint');
 
+
+}
+
+const divs = document.querySelectorAll('.square')
+divs.forEach((div)=>{
+    div.addEventListener('mouseenter', paint);
+});
